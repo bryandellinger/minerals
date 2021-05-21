b8ba290d72b685d5432f959f9f7d9b57283ec9a2
bryandellinger <dellingerfam5@gmail.com>
2021-05-21 08:06:05 -0400

    Add files via upload

---
+++ minerals/Components/CurrentUserComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Minerals.Interfaces;
+
+namespace Minerals.Components
+{
+    public class CurrentUserComponent : ViewComponent
+    {
+        private ICurrentUserRepository repository;
+
+        public CurrentUserComponent(ICurrentUserRepository repo) => repository = repo;
+
+        public IViewComponentResult Invoke() => View(repository.Get(HttpContext, User.Identity.Name));
+    }
+}
---
+++ minerals/Components/MenuComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Minerals.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Minerals.Components
+{
+    public class MenuComponent : ViewComponent
+    {
+        private MenuDataViewModel data;
+        public MenuComponent(MenuDataViewModel d) => data = d;
+        public IViewComponentResult Invoke() => View(data);
+    }
+}
